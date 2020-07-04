4796ff22b128d2316f3037323fdcb11305e159db
egberto.mendonca <egberto.mendonca@accurate.com.br>
2020-07-04 12:08:04 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/estudo/cursomc/domain/ItemPedido.java
+++ src/main/java/com/estudo/cursomc/domain/ItemPedido.java
@@ -1,5 +1,7 @@
 package com.estudo.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import java.io.Serializable;
@@ -9,6 +11,7 @@ import java.util.Objects;
 public class ItemPedido implements Serializable {
     private static final long serialVersionUID = 1l;
 
+    @JsonIgnore
     @EmbeddedId
     private ItemPedidoPK id = new ItemPedidoPK();
 
@@ -26,11 +29,12 @@ public class ItemPedido implements Serializable {
         this.preco = preco;
     }
 
+    @JsonIgnore
     public Pedido getPedido(){
         return id.getPedido();
     }
 
-    public Produto produto(){
+    public Produto getProduto(){
         return id.getProduto();
     }
 
