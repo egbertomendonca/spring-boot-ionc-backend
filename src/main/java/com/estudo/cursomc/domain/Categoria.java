c93799d6597493c40f326e06583995150401cc4f
egberto.mendonca <egberto.mendonca@accurate.com.br>
2020-07-03 22:34:22 -0300

    Banco de dados H2 e criacao automatica de base de dados

--- src/main/java/com/estudo/cursomc/domain/Categoria.java
+++ src/main/java/com/estudo/cursomc/domain/Categoria.java
@@ -5,14 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
