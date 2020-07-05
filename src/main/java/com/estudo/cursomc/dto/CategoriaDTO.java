25f616b3acebf7500dbacc00c26a427b21bf0371
egberto.mendonca <egberto.mendonca@accurate.com.br>
2020-07-05 09:50:57 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/estudo/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/estudo/cursomc/dto/CategoriaDTO.java
@@ -3,13 +3,18 @@ package com.estudo.cursomc.dto;
 import com.estudo.cursomc.domain.Categoria;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 public class CategoriaDTO implements Serializable {
     private static final long serialVersionUID = 1l;
 
     private Integer id;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
     private String nome;
 
     public CategoriaDTO() {}
--- src/main/java/com/estudo/cursomc/services/CategoriaService.java
+++ src/main/java/com/estudo/cursomc/services/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.estudo.cursomc.services;
 
 import com.estudo.cursomc.domain.Categoria;
+import com.estudo.cursomc.dto.CategoriaDTO;
 import com.estudo.cursomc.repositories.CategoriaRepository;
 import com.estudo.cursomc.services.exceptions.DataIntegrityException;
 import com.estudo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -52,6 +53,10 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
         PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
         return repo.findAll(pageRequest);
+    }
+
+    public Categoria fromDTO(CategoriaDTO objDto){
+        return new Categoria(objDto.getId(),objDto.getNome());
     }
 
 }
