77c524a63328f5e9cf5e073ef6b033d0645f7593
egberto.mendonca <egberto.mendonca@accurate.com.br>
2020-07-05 18:13:29 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/estudo/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/estudo/cursomc/dto/ClienteDTO.java
@@ -11,6 +11,7 @@ public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1l;
 
     private Integer id;
+
     @NotEmpty(message = "Preenchimento obrigatório")
     @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
