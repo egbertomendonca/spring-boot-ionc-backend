bbc3ba72908b51d80da79f8641e74433d734593b
egberto.mendonca <egberto.mendonca@accurate.com.br>
2020-07-05 10:38:55 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/estudo/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/estudo/cursomc/dto/ClienteDTO.java
@@ -1,24 +1,30 @@
 package com.estudo.cursomc.dto;
 
-import com.estudo.cursomc.domain.Categoria;
+import com.estudo.cursomc.domain.Cliente;
 import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
-public class CategoriaDTO implements Serializable {
+public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1l;
 
     private Integer id;
-
     @NotEmpty(message = "Preenchimento obrigatório")
-    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
     private String nome;
 
-    public CategoriaDTO() {}
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
 
-    public CategoriaDTO(Categoria obj){
+    public ClienteDTO() {}
+
+    public ClienteDTO (Cliente obj){
         id = obj.getId();
         nome = obj.getNome();
+        email = obj.getEmail();
     }
 
     public Integer getId() {
@@ -35,5 +41,13 @@ public class CategoriaDTO implements Serializable {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
--- src/main/java/com/estudo/cursomc/services/ClienteService.java
+++ src/main/java/com/estudo/cursomc/services/ClienteService.java
@@ -1,11 +1,19 @@
 package com.estudo.cursomc.services;
 
 import com.estudo.cursomc.domain.Cliente;
+import com.estudo.cursomc.domain.Cliente;
+import com.estudo.cursomc.dto.ClienteDTO;
 import com.estudo.cursomc.repositories.ClienteRepository;
+import com.estudo.cursomc.services.exceptions.DataIntegrityException;
 import com.estudo.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +27,38 @@ public class ClienteService {
         return obj.orElseThrow( () -> new ObjectNotFoundException(
                         "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
     }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj , obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return repo.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO objDto){
+        return new Cliente(objDto.getId(),objDto.getNome(), objDto.getEmail(),null,null);
+    }
+
 }
