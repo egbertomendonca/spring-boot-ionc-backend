fab4ab6c589f9ef151222bc50ad3a7eec0ec19b8
egberto.mendonca <egberto.mendonca@accurate.com.br>
2020-07-05 20:27:54 -0300

    Inserindo Pedido

--- src/main/java/com/estudo/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/estudo/cursomc/domain/PagamentoComBoleto.java
@@ -2,12 +2,14 @@ package com.estudo.cursomc.domain;
 
 import com.estudo.cursomc.domain.enums.EstadoPagamento;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import javax.persistence.Entity;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
+@JsonTypeName("pagamentoComBoleto")
 public class PagamentoComBoleto extends Pagamento {
     private static final long serialVersionUID = 1l;
 
---
+++ src/main/java/com/estudo/cursomc/services/BoletoService.java
@@ -0,0 +1,18 @@
+package com.estudo.cursomc.services;
+
+import com.estudo.cursomc.domain.PagamentoComBoleto;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Service
+public class BoletoService {
+
+    public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(instanteDoPedido);
+        cal.add(Calendar.DATE,7);
+        pagto.setDataVencimento(cal.getTime());
+    }
+}
